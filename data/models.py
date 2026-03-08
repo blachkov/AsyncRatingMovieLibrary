15d9af8af3360fe3601ee347debdaee8ea7772e7
blachkov <slaviblachkov@gmail.com>
2026-03-08 19:54:09 +0200

    AddedConstraints

--- data/models.py
+++ data/models.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from typing import Annotated
+
+from pydantic import BaseModel, Field, StringConstraints
 
 class User(BaseModel):
     id: int | None=None
@@ -16,9 +18,9 @@ class User(BaseModel):
     
 class Movie(BaseModel):
     id: int | None=None
-    title: str
-    director: str
-    release_year: int
+    title: Annotated[str, StringConstraints(min_length=1)]
+    director: Annotated[str, StringConstraints(min_length=1)]
+    release_year: Annotated[int,Field(gt=1930,lt=2027)]
     rating: int | None = None
 
     @classmethod
